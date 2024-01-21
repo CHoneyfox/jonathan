6fbb6db83e2916b12faf3743d025220a955b83d9
CHoneyfox <75548757+CHoneyfox@users.noreply.github.com>
2024-01-21 19:23:16 +0100

    added basic string stuff

--- string.h
+++ string.h
@@ -1,10 +1,97 @@
-#ifndef STRING_H_
-#define STRING_H_
+#ifndef JONATHAN_STRING_H_
+#define JONATHAN_STRING_H_
 
-typedef struct string {
+#ifndef MALLOC
+#include <stdlib.h>
+#define MALLOC malloc
+#endif
+
+#ifndef REALLOC
+#include <stdlib.h>
+#define REALLOC realloc
+#endif
+
+#ifndef FREE
+#include <stdlib.h>
+#define FREE free
+#endif
+
+
+#ifndef ASSERT
+#include <assert.h>
+#define ASSERT assert
+#endif
+
+#include <string.h>
+#include <strings.h>
+
+
+#define JSTRING_DEFAULT_CAPACITY 10
+
+
+typedef struct jstring {
     int length;
-    const char* string;
-} string;
+    int capacity;
+    char* string;
+} jstring;
 
 
-#endif // STRING_H_
+jstring jstringFrom(const char* from);
+jstring jstringNew();
+
+void jstringFree(jstring *str);
+
+void jstringAppend(jstring* s, const char* str);
+
+
+#ifdef JONATHAN_STRING_IMPLEMENTATION
+
+jstring jstringFrom(const char* from)
+{
+    jstring s;
+    s.capacity = strlen(from)+1;
+    s.length = s.capacity-1;
+    s.string = (char*)MALLOC(sizeof(char)*s.capacity);
+    strcpy(s.string, from);
+
+    return s;
+}
+
+jstring jstringNew()
+{
+    jstring s;
+    s.capacity = JSTRING_DEFAULT_CAPACITY;
+    s.length = 0;
+    s.string = (char*)MALLOC(sizeof(char)*JSTRING_DEFAULT_CAPACITY);
+
+    return s;
+}
+
+void jstringFree(jstring *str)
+{
+    FREE(str->string);
+}
+
+
+void jstringAppend(jstring* str, const char* add)
+{
+    int additional_size = strlen(add);
+
+    if (additional_size + str->length + 1 > str->capacity)
+    {
+        int new_size = str->length + additional_size + 1;
+        str->string = REALLOC(str->string, sizeof(char)*new_size);
+        str->capacity = new_size;
+
+        for (int i = str->length; i < new_size; i++)
+        {
+            //str->string[i] = 0;
+        }
+    }
+
+    strcpy(str->string+str->length, add);
+    str->length += additional_size;
+}
+
+#endif // JONATHAN_STRING_IMPLEMENTATION
+#endif // JONATHAN_STRING_H_
